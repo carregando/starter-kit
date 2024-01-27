f4e09a638fecd459f44656e0b9c1a09d2abf89d5
carregando <joojnow@gmail.com>
2024-01-27 18:21:18 -0400

    primeiro commit

---
+++ pages/_app.jsx
@@ -0,0 +1,21 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+
+import theme from '../src/theme'
+
+const GlobalStyle = createGlobalStyle`
+* {
+  padding: 0;
+  margin: 0;
+}
+`
+
+function App ({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={theme}>
+     <GlobalStyle/>
+     <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default App
